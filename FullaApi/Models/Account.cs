ef27e40e0cb5e7c816701d8c86fbd88c9a127140
BlueDiablos <jmichaelblackledge@gmail.com>
2022-12-02 22:57:46 -0500

    Initial Commit

---
+++ FullaApi/Models/Account.cs
@@ -0,0 +1,13 @@
+namespace FullaApi.Models;
+
+public class Account 
+{
+    public string EmailAddress { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string MasterPassword { get; set; }
+    
+    public string Hint { get; set; }
+    
+}
